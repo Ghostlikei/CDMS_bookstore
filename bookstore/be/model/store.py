0977e60b09f4a36f4447663a72f6bc721e4048d6
Wu Mianzhi <1427207878@qq.com>
2023-11-01 21:47:43 +0800

    重写store.py，初始化数据库连接

--- bookstore/be/model/store.py
+++ bookstore/be/model/store.py
@@ -1,53 +1,44 @@
 import logging
 import os
 import sqlite3 as sqlite
+from pymongo import MongoClient
 
+# 也许应当移动到配置文件中
+connect_url = "mongodb://userName:daseCDMS2023@127.0.0.1:27017"
 
 class Store:
     database: str
 
     def __init__(self, db_path):
-        self.database = os.path.join(db_path, "be.db")
+        self.database = "be"
         self.init_tables()
 
     def init_tables(self):
-        try:
-            conn = self.get_db_conn()
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS user ("
-                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
-                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
-            )
+        collections = ["user", "store", "order", "order_detail"]
+        client = self.get_db_conn()
+        db = client[self.database]
+        # we use session to manage the transaction and rollback
+        with client.start_session() as session:
+            with session.start_transaction():
+                try:
+                    # create collections if not exist
+                    collections_name = db.list_collection_names()
+                    for collection in collections:
+                        if collection not in collections_name:
+                            logging.info(f"collection {collection} not exist, create it")
+                            db.create_collection(collection)
+                        else:
+                            logging.info(f"collection {collection} already exist")
+                    session.commit_transaction()
+                except Exception as e:
+                    # rollback if error
+                    logging.error(e)
+                    session.abort_transaction()
+                pass
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS user_store("
-                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
-            )
-
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS store( "
-                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
-                " PRIMARY KEY(store_id, book_id))"
-            )
-
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order( "
-                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
-            )
-
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order_detail( "
-                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
-                "PRIMARY KEY(order_id, book_id))"
-            )
-
-            conn.commit()
-        except sqlite.Error as e:
-            logging.error(e)
-            conn.rollback()
-
-    def get_db_conn(self) -> sqlite.Connection:
-        return sqlite.connect(self.database)
+    def get_db_conn(self) -> MongoClient:
+        client = MongoClient(connect_url)
+        return client
 
 
 database_instance: Store = None
