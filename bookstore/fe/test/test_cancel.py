a88712bd5ccf7ff86ebbb41d7644597dbf65caf1
riyad(lx) <10215501402@stu.ecnu.edu.cn>
2023-11-06 12:52:51 +0800

    修改了test_cancel.py和test_confirm.py

--- bookstore/fe/test/test_cancel.py
+++ bookstore/fe/test/test_cancel.py
@@ -1,6 +1,8 @@
 import pytest
 import uuid
 from fe.access.new_buyer import register_new_buyer
+from fe.test.gen_book_data import GenBook
+
 
 
 class TestCancel:
@@ -9,13 +11,20 @@ class TestCancel:
         self.user_id = "test_cancel_{}".format(str(uuid.uuid1()))
         self.password = self.user_id
         self.buyer = register_new_buyer(self.user_id, self.password)
-        self.order_id = "test_cancel_order_id_{}".format(str(uuid.uuid1()))
+        # self.order_id = "test_cancel_order_id_{}".format(str(uuid.uuid1()))
+        self.seller_id = "test_cancel_seller_id_{}".format(str(uuid.uuid1()))
+        self.store_id = "test_cancel_store_id_{}".format(str(uuid.uuid1()))
+        self.gen_book = GenBook(self.seller_id, self.store_id)
+        ok,buy_book_list = GenBook.gen(non_exist_book_id=False, low_stock_level=False)
+        assert ok
+        status_code,self.order_id = self.buyer.new_order(self.store_id,buy_book_list)
+        assert status_code == 200
         yield
 
     def test_ok(self):
         code = self.buyer.cancel(self.order_id)
         assert code == 200
-
+        # todo ;add status
     def test_error_user_id(self):
         self.buyer.user_id = self.buyer.user_id + "_x"
         code = self.buyer.cancel(self.order_id)
--- bookstore/fe/test/test_confirm.py
+++ bookstore/fe/test/test_confirm.py
@@ -1,7 +1,7 @@
 import pytest
 import uuid
 from fe.access.new_buyer import register_new_buyer
-
+from fe.test.gen_book_data import GenBook
 
 class TestConfirm:
     @pytest.fixture(autouse=True)
@@ -9,7 +9,14 @@ class TestConfirm:
         self.user_id = "test_confirm_{}".format(str(uuid.uuid1()))
         self.password = self.user_id
         self.buyer = register_new_buyer(self.user_id, self.password)
-        self.order_id = "test_confirm_order_id_{}".format(str(uuid.uuid1()))
+        # self.order_id = "test_confirm_order_id_{}".format(str(uuid.uuid1()))
+        self.seller_id = "test_confirm_seller_id_{}".format(str(uuid.uuid1()))
+        self.store_id = "test_confirm_store_id_{}".format(str(uuid.uuid1()))
+        self.gen_book = GenBook(self.seller_id, self.store_id)
+        ok, buy_book_list = GenBook.gen(non_exist_book_id=False, low_stock_level=False)
+        assert ok
+        status_code, self.order_id = self.buyer.new_order(self.store_id, buy_book_list)
+        assert status_code == 200
         yield
 
     def test_ok(self):
