67a98e26d211e5e87dca13278e04c66dc95002d4
Wu Mianzhi <1427207878@qq.com>
2023-11-06 21:03:17 +0800

    Search v1

--- bookstore/be/model/search.py
+++ bookstore/be/model/search.py
@@ -7,5 +7,29 @@ class Search(db_conn.DBConn):
         db_conn.DBConn.__init__(self)
         
     def search(self, parameters, page, result_per_page):
-        # todo!
-        return 200, "test", "test"
+        store_collection = self.db["store"]
+        # parse parameters
+        store_id = parameters.get("scope", None)
+        title = parameters.get("title", None)
+        tags = parameters.get("tags", None)
+        catalog = parameters.get("catalog", None)
+        content = parameters.get("content", None)
+        
+        # build query
+        condition_list = []
+        if store_id is not None:
+            condition_list.append({"store_id": store_id})
+        if title is not None:
+            condition_list.append({"title": title})
+        if tags is not None:
+            condition_list.append({"tags": tags})
+        if catalog is not None:
+            condition_list.append({"catalog": catalog})
+        if content is not None:
+            condition_list.append({"content": content})
+        if len(condition_list) == 0:
+            return error.error_empty_search_parameters()
+        query = {"$and": condition_list}
+        print(query)
+        results = list(store_collection.find(query, {"_id": 0, "owner": 0}).skip((page - 1) * result_per_page).limit(result_per_page))
+        return 200, "ok", results
